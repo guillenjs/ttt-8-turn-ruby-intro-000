15efeae9a9ec657defff00303bc4b079b7649913
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-06 23:27:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,7 @@ def move(board, index, token = "X")
   board[index] = token
 end
 
-def turn(board, user_input)
+def turn(board)
   puts "Please enter 1-9:"
   user_input = input_to_index(user_input)
 end
